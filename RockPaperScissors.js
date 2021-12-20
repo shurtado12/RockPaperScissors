64144e91d705506d1c6fb8046e3e9ce868629ebe
shurtado12 <92539599+shurtado12@users.noreply.github.com>
2021-12-19 23:33:15 -0500

    Add files via upload
    Trying to add a loop to rng and implement the nested function "game".

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -3,35 +3,9 @@
 
 
 
-let playerScore = 0; //score starts at zero for both player and cpu
-let cpuScore = 0;
-let playerSelection = prompt("Rock, Paper or Scissors?"); //player input into text box determines the player choice
-let computerSelection = computerPlay(); //cpu selection is determined by the function computerPlay (random)
 
 
-function computerPlay() {// Random selection for CPU 
 
-    
-    const selection = ["rock", "paper", "scissors"];
-  
-    const randomItem = [Math.floor(Math.random() * selection.length)];
-     return (randomItem, selection[randomItem])
-    
-}
-
-
-/* These blocks have been commented out because they are improperly implemented.
-}else if ( playerScore < cpuScore)
-
-{console.log( "Sorry! You Lose! Game Over!");
-
-}else if (playerScore === cpuScore)
-{console.log ("Draw! Game Over!");
-
-   }*/
-/*function game (playerSelection, computerSelection){
-    {
-*/
         
 function playRound(playerSelection, computerSelection) {//player selection to lower case makes player input case insensitive.
 // This function contains the code to run a single round of Rock Paper Scissors.
@@ -42,27 +16,32 @@ function playRound(playerSelection, computerSelection) {//player selection to lo
         return "Draw!"; 
 
     } else if(playerSelection.toLowerCase()   === "rock" && computerSelection === "paper") {//player loss increments cpu score by one
+        ++roundNumber;
         ++cpuScore;   
         return ["Sorry! You Lose! Paper beats Rock!"];
 
     } else if ( playerSelection.toLowerCase()  === "paper" && computerSelection === "scissors") {
-         ++cpuScore;  
+        ++roundNumber;
+        ++cpuScore;  
         return ["Sorry! You Lose! Scissors beats Paper!"] ;
 
     } else if ( playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
-         ++cpuScore;
+        ++roundNumber;
+        ++cpuScore;
         return ["Sorry! You Lose! Rock beats Scissors!"]  ;
                                                            //Player wins increment player score by 1.             
     } else if ( playerSelection.toLowerCase() === "rock"  && computerSelection === "scissors") {
+        ++roundNumber;
         ++playerScore;  
         return ["You Win! Rock beats Scissors!"]  ;
 
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+        ++roundNumber;
         ++playerScore;    
         return ["You Win! Paper beats Rock!"]   
 
     } else if ( playerSelection.toLowerCase() === "scissors" && computerSelection ==="paper")  {
-        
+        ++roundNumber;
         ++playerScore;  
         return ["You Win! Scissors beats Paper!"] ;
 
@@ -72,19 +51,46 @@ function playRound(playerSelection, computerSelection) {//player selection to lo
         }
 
 
-    
+        
         
 }    /*Tlhis is me trying to figure out how to loop the rounds and rng for enemy selection.
 */
     
-for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerSelection) +" " + "CPU chose" + " " + computerSelection + //prints plays and scores
-    ". Current Scores - Player Score: " + " " + playerScore + " " + "CPU Score: " + cpuScore);
-    prompt();
-     computerPlay();
-}
+let playerScore = 0; //score starts at zero for both player and cpu
+let cpuScore = 0;
+let roundNumber = 0;
+
+let playerSelection = prompt("Rock, Paper or Scissors?"); //player input i nto text box determines the player choice
+let computerSelection = computerPlay(); //cpu selection is determined by the function computerPlay (random)
+
+
      
+function computerPlay() {// Random selection for CPU 
+
     
+    const selection = ["rock", "paper", "scissors"];
+    
+    
+
+    const randomItem = [Math.floor(Math.random() * selection.length)];
+
+      
+    return (randomItem, selection[randomItem]);
+    
+}
+function game(){
+for  (let i = 0; i < 5; i++) {
+   
+    computerPlay();
+    prompt();
+   return (playRound(playerSelection, computerSelection) +" " + "CPU chose" + " " + computerSelection + //prints plays and scores
+        ". Current Scores - Player Score: " + " " + playerScore + " " + "CPU Score: " + cpuScore + " " + "Round: " + roundNumber );
+
+}
+    
+}
+ game ();
+    console.log(game)
 
 /*console.log ("Game Over! Final Scores Player Score:" + playerScore + " CPU Score:" + cpuScore)*/
 
